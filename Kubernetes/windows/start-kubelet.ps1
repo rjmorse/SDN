f2576e0ab1b283aff0754486d280622c1c215967
Robert Morse <robert.morse@ticketmaster.com>
2017-12-13 22:23:15 -0800

    add missing service info

--- Kubernetes/windows/start-kubelet.ps1
+++ Kubernetes/windows/start-kubelet.ps1
@@ -1,3 +1,7 @@
+# Todo : Get these values using kubectl
+$KubeDnsServiceIp="11.0.0.10"
+$serviceCIDR="11.0.0.0/8"
+
 c:\k\kubelet.exe --hostname-override=$(hostname) --v=6 `
     --pod-infra-container-image=kubeletwin/pause --resolv-conf="" `
     --allow-privileged=true --enable-debugging-handlers `
